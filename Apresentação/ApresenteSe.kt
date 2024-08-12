f6e5925fef38a15c7256387e1a3d4373ac9bc53a
RVidaDev <vidraf9@gmail.com>
2024-08-12 20:23:03 -0300

    apresentação

---
+++ Apresentação/ApresenteSe.kt
@@ -0,0 +1,12 @@
+//programa pra se apresentar na aula
+
+fun main() {
+
+    var idade = 19      //minha idade
+    var altura = 1.80   //minha altura (f indica float)
+    var inicial = 'R'   //inicial do meu nome
+
+    //impressão das informações usando interpolação
+    println("Mnha inicial é $inicial, tenho $idade anos e minha altura é %.2f".format(altura))
+
+}
