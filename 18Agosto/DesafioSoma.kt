91079084f9e47e70dafbb9751189524410801d66
RVidaDev <vidraf9@gmail.com>
2024-08-12 20:45:22 -0300

    função

---
+++ 18Agosto/DesafioSoma.kt
@@ -0,0 +1,10 @@
+fun somaDosQuadrados(a: Int, b: Int) : Int {
+
+    val Aquadrado = a + a
+    val Bquadrado = b + b
+
+    resultado = Aquadrado + Bquadrado
+
+    println("a soma dos quadrados de %a e %b é %resultado")
+
+}
